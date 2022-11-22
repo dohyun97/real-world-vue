c680b4c6a1edba3124d01bafd5eaf23d9677bf79
dohyun97 <dohyun647@gmail.com>
2022-11-21 22:04:27 -0500

    availble to clike event cards

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
+import EventDetails from "../views/EventDetails.vue";
 import About from "../views/AboutView.vue"; 
 const routes = [
   {
     path: "/",
     name: "EventList",
-    component: EventList,
+    component: EventList
   },
   {
+    path: "/event/:id ",
+    name: "EventDetails",
+    props: true,
+    component: EventDetails
+  }, 
+  {
     path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    name: "About",
     component: About
-  },
+  }
 ];
 
 const router = createRouter({
